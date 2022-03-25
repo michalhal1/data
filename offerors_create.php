c820659c5ba6eb4245e4fa7d560abb3f12e8451b
Michal Halama <m.halama@impel.pl>
2022-03-25 11:17:27 +0100

    zmiana nagłówków

--- offerors_create.php
+++ offerors_create.php
@@ -276,7 +276,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <head>
     <meta charset="UTF-8">
-    <title>Nowy przetarg</title>
+    <title>Nowa oferta</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         .wrapper {
