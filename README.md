ae8364e1f232b8a398d33b01e9abfa285618312c
michal <mic.hal@hotmail.com>
2022-08-23 22:54:42 +0200

    first commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# baza_przetargowa
-
-Plik config.php zawiera połączenie z bazą tenders_test. Pliku nie należy kopiować na boiws.impel.pl podczas aktualizacji. .
--- update_tablespy.py
+++ update_tablespy.py
@@ -1,32 +1,18 @@
 import MySQLdb
 import pandas as pd
 host = '10.255.134.193'
-user ='boiws'
-passwd = 'OtherOddsUpdateRank73'
-db = 'bids_test'
+user ='user'
+passwd = 'password'
+db = 'database'
 
 db_connection = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db)
 cor = db_connection.cursor()
 
-
-
-
-df = pd.read_excel(r"C:\Users\mhal04\Documents\BazaPB.xlsx")
-
-df1 = df.reset_index()
-
-df = df.truncate(before = 121379, after = 144985)
-
+df = pd.read_excel(r"C:\Users\mhal04\Documents\excel_file.xlsx")
 df = df.reset_index()
 
-df = df.loc[:, "index":"Rola"]
-
-#print(df)
-#print(df1)
-
-
 for index, dane in df.iterrows():
-    sql = "insert into client_CRM_data ( client_name, client_NIP, client_PKD_nr, client_PKD_name, client_city, client_postal_code, client_street, client_house_number, client_role) values ('" + (str(dane[2])).strip() + "','" + (str(dane[3])).strip() + "','" + (str(dane[4])).strip() + "','" + (str(dane[5])).strip() + "','" + (str(dane[6])).strip() + "','" + (str(dane[7])).strip()  + "','" + (str(dane[8])).strip() + "','" + (str(dane[9])).strip() +  "','" + (str(dane[10])).strip() + "'"  + ')'
+    sql = "insert into table ( col1, col2, col3, col4, col5, col6, col7, col8, col9) values ('" + (str(dane[2])).strip() + "','" + (str(dane[3])).strip() + "','" + (str(dane[4])).strip() + "','" + (str(dane[5])).strip() + "','" + (str(dane[6])).strip() + "','" + (str(dane[7])).strip()  + "','" + (str(dane[8])).strip() + "','" + (str(dane[9])).strip() +  "','" + (str(dane[10])).strip() + "'"  + ')'
    
     cor.execute(sql)
     #print(sql)
